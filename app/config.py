f71361c59fe4b3bad03428beaeb11f4404c5db2a
margaretpell <95526715+margaretpell@users.noreply.github.com>
2023-04-19 00:09:45 -0400

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,7 +1,5 @@
 class Config(object):
     SECRET_KEY = 'asdfg'
-    # SQLALCHEMY_ECHO = False
-    # SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_EXTENSIONS = {'.jpg', '.jpeg', '.png', '.gif'}
     MAX_CONTENT_LENGTH = 16 * 1000 * 1000
     S3BUCKET = 'ece1779a3bucket1'
